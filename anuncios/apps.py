e22405d56b0bb518bc9c4c50b8f2b40658771339
Giovana Torres <giovanatorresls@gmail.com>
2026-04-22 21:08:37 -0300

    Adição de anuncios

--- anuncios/apps.py
+++ anuncios/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class VeiculoConfig(AppConfig):
+class AnuncioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'veiculo'
+    name = 'anuncios'
